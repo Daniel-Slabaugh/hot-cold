7653f33b99ca9e73058dd8f9e6c7d8d2c92cc8da
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-12-20 17:20:32 -0600

    fixed index.js by adding game

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Game from './components/game';
+
 
 ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
